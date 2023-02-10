628dbe7376f92f10455c45ab8119b7d7182c3563
CodingManoj <mvp2612@hotmail.com>
2023-02-09 20:00:02 -0500

    AutoPush

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -17,3 +17,18 @@ output "print-demo" {
 # Note : A variable can be accessed without ${} as well, only if its the only declared entry in the value part of the output. 
         // If you're using the variable in between the strings, you need to enclose them in the quotes and ${} . 
         // There is no concept of single quotes `'` in terraform
+
+
+variable "example-list" {
+    default = [
+        "AWS",
+        "DevOps",
+        true,
+        100,
+        "SRE"
+    ]
+}
+
+output "ex-list-op" {
+    value = "Welcome to ${var.example-list[0]} and ${var.example-list[1]} Training, Training durations is ${var.example-list[3]} hours , we can also apply for ${var.example-list[4]} jobs"
+}
