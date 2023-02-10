cc5763aee9492080a787b6870bb07c6625ee654a
CodingManoj <mvp2612@hotmail.com>
2023-02-09 20:50:55 -0500

    AutoPush

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # creates ec2 instance
 resource "aws_instance" "this" {
   ami                     = "ami-0c1d144c8fdd8d690"
-  instance_type           = "t3.micro"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "Terraform-Server"
