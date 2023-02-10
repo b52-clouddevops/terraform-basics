6e69c228fd43b56ba7c32b2235fdb98b1ecbbfb1
CodingManoj <mvp2612@hotmail.com>
2023-02-09 20:05:03 -0500

    AutoPush

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -19,6 +19,7 @@ output "print-demo" {
         // There is no concept of single quotes `'` in terraform
 
 
+# This is how we declare a list variable
 variable "example-list" {
     default = [
         "AWS",
@@ -31,4 +32,18 @@ variable "example-list" {
 
 output "ex-list-op" {
     value = "Welcome to ${var.example-list[0]} and ${var.example-list[1]} Training, Training durations is ${var.example-list[3]} hours , we can also apply for ${var.example-list[4]} jobs"
+}
+
+
+# This is how we declare a map variable 
+variable "ex-map" {
+  default = {
+        Duration = 105, 
+        Trainer = "Verma", 
+        Cloud = "AWS"
+  }
+}
+
+output "ex-map-op" {
+    value = "Welcome to DevOps with ${var.ex-map["Cloud"]} Training and the training duration is ${var.ex-map["Duration"]} , training name is ${var.ex-map["Trainer"]}"
 }
