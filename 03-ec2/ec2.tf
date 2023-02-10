434cc1ede622267dcbda0991ce68411f548042fc
CodingManoj <mvp2612@hotmail.com>
2023-02-09 20:54:02 -0500

    AutoPush

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # creates ec2 instance
 resource "aws_instance" "this" {
-  ami                     = "ami-0c1d144c8fdd8d690"
+  ami                     = "ami-0e3aeafe193bbdd4a"
   instance_type           = "t2.micro"
 
   tags = {
