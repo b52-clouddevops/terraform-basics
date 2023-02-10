fa2260ea34d34ea8ca80a3b4448a3e43f03ef607
CodingManoj <mvp2612@hotmail.com>
2023-02-09 20:15:26 -0500

    AutoPush

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -11,4 +11,10 @@ output "state-name" {
     value = "Name of the state is ${var.State}"
 }
 
+variable "Project" {}
+
+output "project-name" {
+    value = "Name of the project is ${var.Project}"
+}
+
 # Always ensure to delcare an empty variable before you use it.
