a02479a2775d222245ac24cb2b048f810366eb31
CodingManoj <mvp2612@hotmail.com>
2023-02-10 19:52:17 -0500

    AutoPush

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -20,9 +20,17 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow SSH inbound traffic"
 
   ingress {
-    description = "SSH from SSH"
+    description = "SSH from Public"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
+  }
+
+  ingress {
+    description = "HTTP from Public"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
   }
