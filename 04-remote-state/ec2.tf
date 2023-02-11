eafab3c2b02c044653d62c7f3c9a292eeae33e5b
CodingManoj <mvp2612@hotmail.com>
2023-02-10 20:30:35 -0500

    AutoPush

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -1,6 +1,6 @@
 # creates ec2 instance
 resource "aws_instance" "my-ec2" {
-  ount                   = 3c
+  ount                   = 3
   ami                     = "ami-0e3aeafe193bbdd4a"
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
