1b197cb30beaa6882fd20426c437a34b85f1da65
CodingManoj <mvp2612@hotmail.com>
2023-02-12 20:07:13 -0500

    AutoPush

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "my-ec2" {
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.sg]
 
-
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
@@ -33,3 +32,5 @@ output "public_ip_address" {
 # How do we patch the AMI ???
 ### Answer : Make an instance out of that AMI, update the server with latest pathces; Then , make AMI out of it with the same name and then deresigter the old AMI.
 #### Airlines-Proj-X-1-Feb ;  Airlines-Proj-X-2-Mar ;
+
+# PS : If you keep the provisioner inside the resource, if the provisioner fails for any reason, it assumes that the whole block is a failure and then it destroy and recreates in the next run.
