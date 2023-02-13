f94549ba752b04d421334a1e263cbcb17e2b08c7
CodingManoj <mvp2612@hotmail.com>
2023-02-12 21:08:04 -0500

    AutoPush

--- robot-project/ec2/route53.tf
+++ robot-project/ec2/route53.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "component" {
   zone_id = "Z090521761DHPU3HXLNP"
   name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "A"
-  ttl     = 300
+  ttl     = 10
   records = [aws_spot_instance_request.my_spot_server.private_ip]
 }
