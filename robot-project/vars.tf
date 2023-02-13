d11f69244863228de4cb15ee2ebe0cbd82df9ce1
CodingManoj <mvp2612@hotmail.com>
2023-02-12 21:03:40 -0500

    AutoPush

--- robot-project/vars.tf
+++ robot-project/vars.tf
@@ -33,32 +33,3 @@ variable "COMPONETS" {
         }
     }
 }
-
-
-
-
- module "mongodb" {
-    source      = "./ec2"
-    COMPONENT   = "mongodb"
-    APP_VERSION = "null"
-}
-
-
-
-module "rabbitmq" {
-    source      = "./ec2"
-    COMPONENT   = "rabbitmq"
-    APP_VERSION = "0.0.9"
-}
-
-module "payment" {
-    source      = "./ec2"
-    COMPONENT   = "payment"
-    APP_VERSION = "0.0.2"
-}
-
-module "frontend" {
-    source      = "./ec2"
-    COMPONENT   = "frontend"
-    APP_VERSION = "0.0.1"
-}
