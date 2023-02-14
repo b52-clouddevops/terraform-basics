30d9461c933ab7bf58674ae059089802d4667a7a
CodingManoj <mvp2612@hotmail.com>
2023-02-13 19:45:36 -0500

    AutoPush

--- robot-project/ec2/main.tf
+++ robot-project/ec2/main.tf
@@ -26,12 +26,7 @@
 #     }
 # }
 
-
-
-
-
-
-
+# Creates on-demand instances
 resource "aws_instance" "my-ec2" {
   ami                     = data.aws_ami.lab-image.image_id
   instance_type           = "t2.micro"
